e1188d852e1a450805405b44065b3651c79838ac
James <j.clegg1@hotmail.co.uk>
2019-07-16 11:33:32 +0100

    added mocha hooks to app.spec.js and created the router folder and files

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,16 +1,12 @@
 process.env.NODE_ENV = "test";
 const { expect } = require("chai");
-const require = require("supertest");
+const request = require("supertest");
 const app = require("../app");
-const connection = require('../db/connection')
+const connection = require("../db/connection");
 
-
-describe('/api', () => {
+describe("/api", () => {
   after(() => connection.destroy());
-  beforeEach(() => {
-    
-  });
-  it('', () => {
-    
-  });
+  beforeEach(() => connection.seed.run);
+
+
 });
