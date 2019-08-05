231f1828fd9613d67c4ab96e5af9d33743e68063
James <j.clegg1@hotmail.co.uk>
2019-08-05 14:24:17 +0100

    added cors

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api-router");
+var cors = require("cors");
 const {
   handleNonExistentRoutes,
   handleCustomErrors,
   handlePsqlErrors,
   handleServerErrors
 } = require("./errors");
+app.use(cors());
+app.get("/products/:id", function(req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
 
-app.use(express.json())
+app.use(express.json());
 app.use("/api", apiRouter);
 
 app.all("/*", handleNonExistentRoutes);
